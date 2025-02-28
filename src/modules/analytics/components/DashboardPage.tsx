fcb58f7cd5ca242c590005a20a1bb752fa56f761
amalari <student.achmad@gmail.com>
2025-02-28 14:25:31 +0700

    recent transaction component (#22)

--- src/modules/analytics/components/DashboardPage.tsx
+++ src/modules/analytics/components/DashboardPage.tsx
@@ -1,10 +1,12 @@
 import { MyBalanceCards } from "../../balance/components/MyBalanceCards";
+import { RecentTransactions } from "../../transaction/components/RecentTransactions";
 
 export const DashboardPage = () => {
   return (
     <div className="soar-dashboard-page pl-4 md:px-10 py-7 w-full lg:w-[calc(100vw-250px)]">
       <div className="grid grid-cols-3 gap-7">
         <MyBalanceCards className="col-span-3 xl:col-span-2" />
+        <RecentTransactions className="col-span-3 xl:col-span-1" />
       </div>
     </div>
   );
