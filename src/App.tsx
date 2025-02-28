72d18fd58f286456af90ef0f51877da826904c5a
amalari <student.achmad@gmail.com>
2025-02-28 19:27:48 +0700

    routing lazy load page and adjust size weekly activity chart (#32)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
-import { DashboardPage } from './modules/analytics/components/DashboardPage';
+import { useEffect, Suspense, lazy } from 'react';
 import { Layout } from './modules/layout/components/Layout'
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
-import { SettingPage } from './modules/setting/components/SettingPage';
+
+const DashboardPage = lazy(() => import('./modules/analytics/components/DashboardPage').then(module => ({ default: module.DashboardPage })));
+const SettingPage = lazy(() => import('./modules/setting/components/SettingPage').then(module => ({ default: module.SettingPage })));
 
 function App() {
   return (
@@ -10,8 +11,22 @@ function App() {
       <Routes>
         <Route path="/" element={<RedirectToDashboard />} />
         <Route element={<Layout />}>
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="setting" element={<SettingPage />} />
+          <Route 
+            path="dashboard" 
+            element={
+              <Suspense fallback={<>...</>}>
+                <DashboardPage />
+              </Suspense>
+            }
+          />
+          <Route 
+            path="setting" 
+            element={
+              <Suspense fallback={<>...</>}>
+                <SettingPage />
+              </Suspense>
+            }
+          />
         </Route>
         <Route path="*" element={<RedirectToDashboard />} />
       </Routes>
