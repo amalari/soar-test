1839dbe742459d62c70c477dc81c63fd6d7e4d2f
amalari <student.achmad@gmail.com>
2025-03-01 16:56:18 +0700

    Update SettingPage.tsx

--- src/modules/setting/components/SettingPage.tsx
+++ src/modules/setting/components/SettingPage.tsx
@@ -9,7 +9,7 @@ export const SettingPage = () => {
     <div className="soar-setting-page m-3 md:mx-10 md:my-7 p-4 md:p-7 bg-white">
       <Tabs value={activeTab} onValueChange={setActiveTab}>
         <Tabs.List className="gap-7 md:gap-14">
-          <Tabs.Trigger value="profile" className="text-sm md:text-base">Edit Profile</Tabs.Trigger>
+          <Tabs.Trigger value="profile" className="text-sm md:text-base truncate">Edit Profile</Tabs.Trigger>
           <Tabs.Trigger value="preferences" className="text-sm md:text-base">Preferences</Tabs.Trigger>
           <Tabs.Trigger value="security" className="text-sm md:text-base">Security</Tabs.Trigger>
         </Tabs.List>
